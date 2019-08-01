d92c473fe5670bbb1e4def7610d6890c2e356214
smilejsu82 <50188795+smilejsu82@users.noreply.github.com>
2019-08-01 11:37:45 +0900

    create clan

--- node064/rest-api-server.js
+++ node064/rest-api-server.js
@@ -17,11 +17,35 @@ app.get('/', (req, res)=>{
     res.end('restAPI서버 정상 작동 중');
 });
 
+app.get('/clan', (req, res)=>{
+    const pool = mysql_dbc.pool();
+    mysql_dbc.connectPool(pool, (err, con)=>{
+        if(err) {
+            con.release();
+            throw err;
+        }else{
+            let query = "select * from clans";
+            con.query(query, (err, results, fields)=>{
+
+                console.log(results);
+
+                con.release();
+                if(err) { throw err; }
+                res.json({
+                    cmd: 200,
+                    clanList: results
+                });
+            });
+        }
+    });
+});
+
 app.post('/clan', (req, res)=>{
     //clan_code, clan_name, clan_master_id, clan_desc, DATE 
     const clanCode = clan.getClanCode(6);
     const clanName = req.body.clanName;
     const uid = req.body.uid;
+    const userName = req.body.userName;
     const clanDesc = req.body.clanDesc;
     const now = moment().format().slice(0, 19).replace('T', ' ');
     let arrQueryData = [clanCode, clanName, uid, clanDesc, now];
@@ -29,7 +53,7 @@ app.post('/clan', (req, res)=>{
     mysql_dbc.connectPool(pool, (err, con)=>{
         if(err){
             console.error(err);
-            pool.releaseConnection();
+            pool.release();
             res.json({
                 cmd: 9988
             });
@@ -37,41 +61,55 @@ app.post('/clan', (req, res)=>{
             console.info('connected!');
             //쿼리 실행 
             con.beginTransaction((err)=>{
-                if(err) throw err;
-                let query = 'INSERT INTO clans ( clan_code, clan_name, clan_master_id, clan_desc, DATE ) VALUES(?,?,?,?,?)';    
-                con.query(query, arrQueryData, (error, results, fields)=>{
-                    if(error){
+                if(err) {
+                    con.release();
+                    throw err;
+                }
+                let query = 'insert into clan_members(clan_code, clan_member_id, clan_member_name, date) values(?,?,?,?)';
+                con.query(query, [clanCode, uid, userName, now], (err, results, fields)=>{
+                    if(err) {
                         return con.rollback(()=>{
-                            con.releaseConnection();
+                            con.release();
                             throw error;       
                         });
                     }
-                    query = 'select * from clans';    
-                    con.query(query, (error, results, fields)=>{
+                    query = 'INSERT INTO clans ( clan_code, clan_name, clan_master_id, clan_desc, DATE ) VALUES(?,?,?,?,?)';    
+                    con.query(query, arrQueryData, (error, results, fields)=>{
                         if(error){
                             return con.rollback(()=>{
-                                con.releaseConnection();
-                                throw error;
+                                con.release();
+                                throw error;       
                             });
                         }
-                        con.commit((err)=>{
-                            if(err){
+                        query = 'select * from clans';    
+                        con.query(query, (error, results, fields)=>{
+                            if(error){
                                 return con.rollback(()=>{
-                                    con.releaseConnection();
-                                    throw err;
+                                    con.release();
+                                    throw error;
                                 });
                             }
-                            console.log('success');
-                            con.releaseConnection();
-
-                            res.json({
-                                cmd: 200,
-                                clanList: results
+                            con.commit((err)=>{
+                                if(err){
+                                    return con.rollback(()=>{
+                                        con.release();
+                                        throw err;
+                                    });
+                                }
+                                console.log('success');
+                                con.release();
+    
+                                res.json({
+                                    cmd: 200,
+                                    clanList: results
+                                });
+    
                             });
-                            
                         });
                     });
                 });
+
+                
             });
         }
     });
